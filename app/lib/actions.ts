0a69c2a9f4a345c2c9c75902079891f1ed5e6265
Itapeck <itapeck@gmail.com>
2025-05-03 01:45:37 -0300

    Nextjs_final_capitulo_17 - criação de invoices e login

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -64,6 +64,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
     `;
     } catch (error) {
         // If a database error occurs, return a more specific error.
+        console.error('Error inserting invoice:', error);
         return {
             message: 'Database Error. Failed to create invoice.',
         };
@@ -102,6 +103,7 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
     `;
     }
     catch (error) {
+        console.error('Error updating invoice:', error);
         return { message: 'Database Error. Failed to update invoice.' };
     }
 
